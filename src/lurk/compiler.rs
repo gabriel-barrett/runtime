a3ba3d7767c8b1389e96a811275738e5281e2222
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2024-02-17 13:55:55 -0300

    compiler progress

--- src/lurk/compiler.rs
+++ src/lurk/compiler.rs
@@ -32,13 +32,39 @@ fn compile_module(module: &Module) -> CoroutineModule {
     }
     let mut toplevel = HashMap::new();
     for (name, def) in module.toplevel().iter() {
-        let coroutine = compile_definition(def, &index_map);
+        let coroutine = compile_definition(def, name, &index_map);
         let index = index_map.get(name).unwrap();
         toplevel.insert(*index, coroutine);
     }
     CoroutineModule { toplevel }
 }
 
-fn compile_definition(_def: &Definition, _index_map: &HashMap<&String, usize>) -> Coroutine {
+fn compile_definition(
+    def: &Definition,
+    name: &String,
+    index_map: &HashMap<&String, usize>,
+) -> Coroutine {
+    let query_index = *index_map.get(name).unwrap();
+    let name = name.clone();
+    let body = compile_expression(&def.body, index_map);
+    let slots_count = body.count_slots();
+    let input_params = def.params.iter().map(|x| Var::new(x)).collect();
+    let func = Func {
+        name,
+        input_params,
+        // TODO
+        output_size: 0,
+        body,
+        slots_count,
+    };
+    Coroutine {
+        func,
+        query_index,
+        rc: 1,
+        leaf: false,
+    }
+}
+
+fn compile_expression(body: &Expression, index_map: &HashMap<&String, usize>) -> Block {
     todo!()
 }
