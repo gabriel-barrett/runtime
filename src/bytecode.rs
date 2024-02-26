332fd3a4d387d2a30f6ac26132d2858b3bb97f9c
Gabriel Barreto <gabriel.aquino.barreto@gmail.com>
2024-02-26 09:50:20 -0300

    Progress: call compilation

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -6,6 +6,7 @@ use crate::{
 type REG = u32;
 type NUM = u64;
 
+use Bytecode::*;
 pub enum Bytecode {
     OP(Operation, REG, REG, REG),
     OPI1(Operation, REG, NUM, REG),
@@ -13,6 +14,7 @@ pub enum Bytecode {
     CALL(REG),
     CALLI(NUM),
     ARG(NUM, REG),
+    ARGI(NUM, NUM),
     RETURN(REG),
     RETURNI(NUM),
     BRANCH(NUM),
@@ -32,15 +34,47 @@ fn compile_module(module: &Module) -> Vec<Bytecode> {
 fn compile_expression(
     expr: &Expression,
     code: &mut Vec<Bytecode>,
-    mut reg_map: impl FnMut(&String) -> REG,
+    rec_name: &str,
+    rec_pos: usize,
+    reg_map: &mut impl FnMut(&String) -> REG,
+    top_map: &mut impl FnMut(&String) -> usize,
 ) {
-    use Bytecode::*;
     match expr {
         Expression::Unit(Atom::Lit(s)) => code.push(RETURNI(*s)),
         Expression::Unit(Atom::Var(s)) => {
             let r = reg_map(s);
             code.push(RETURN(r))
         }
+        Expression::Call(name, atoms) => {
+            let func = if name == rec_name {
+                rec_pos as NUM
+            } else {
+                top_map(name) as NUM
+            };
+            push_arguments(atoms, code, reg_map);
+            code.push(CALLI(func))
+        }
         _ => todo!(),
+    }
+}
+
+fn push_arguments(
+    args: &[Atom],
+    code: &mut Vec<Bytecode>,
+    reg_map: &mut impl FnMut(&String) -> REG,
+) {
+    for (i, arg) in args.iter().enumerate() {
+        match arg {
+            Atom::Var(s) => {
+                let pos = i as NUM;
+                let reg = reg_map(s);
+                code.push(ARG(pos, reg))
+            }
+            Atom::Lit(x) => {
+                let pos = i as NUM;
+                let val = *x as NUM;
+                code.push(ARGI(pos, val))
+            }
+        }
     }
 }
